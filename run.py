97f7d57410cff93cbaf5a7015530082228dd2460
wold21 <wol2180@gmail.com>
2025-05-13 13:26:15 +0900

    updated: env import 제거 BaseSetting이 가져옴

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 import uvicorn
-from dotenv import load_dotenv
-import os
-
-load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
 from app.core.config import settings
 
 if __name__ == "__main__":
