49fa0e38cdc514ff4f3b278b4d2caa71330ebeaa
hyuk <wold2180@gmail.com>
2025-07-04 16:06:49 +0900

    update: 테스트 시 코드 커버리지 옵션 제거

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "🧪 Running tests..."
 
-pytest app/tests/ -v --tb=short --cov=app
+pytest app/tests/ -v --tb=short
+# --cov=app
+# 코드 커버리지 사용할 시 추가하면 되는데 실행 환경에 모두 pytest-cov를 설치해야함.
 
 if [ $? -eq 0 ]; then
     echo "All tests passed!"
