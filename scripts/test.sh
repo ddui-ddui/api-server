00d2a3e0d5b6ddb74d734383463dd1269f0d5c30
hyuk <wold2180@gmail.com>
2025-07-04 16:17:43 +0900

    update: 테스트 시 라이브러리 설치

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-echo "🧪 Running tests..."
+echo "Running tests..."
 
+# 라이브러리 설치
+echo "Installing dependencies..."
+pip3 install --upgrade pip
+pip3 install -r app/requirements.txt
+
+# 테스트
+echo "Running tests..."
 pytest app/tests/ -v --tb=short
 # --cov=app
 # 코드 커버리지 사용할 시 추가하면 되는데 실행 환경에 모두 pytest-cov를 설치해야함.
