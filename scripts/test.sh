1d126a95546df075fd5238c339aa83f72d764ac7
hyuk <wold2180@gmail.com>
2025-07-05 12:55:45 +0900

    test: test 코드 테스트

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ pip3.11 install -r requirements.txt
 
 # 테스트
 echo "Running tests..."
-pytest app/tests/ -v --tb=short --allow-empty-collection
+pytest app/tests/ -v --tb=short
 # --cov=app
 # 코드 커버리지 사용할 시 추가하면 되는데 실행 환경에 모두 pytest-cov를 설치해야함.
 
