e667df97baeaa04bbb5aee2cc218af685e6362e5
hyuk <wold2180@gmail.com>
2025-07-30 08:52:14 +0900

    update: 좌표 기본 값 소수점 3자리로 변경

--- app/api/endpoints/walkability.py
+++ app/api/endpoints/walkability.py
@@ -12,8 +12,8 @@ logger = get_logger()
 
 @router.get("/current")
 async def get_walkability_current (
-    lat: float = Query(37.4996992, description="위도"),
-    lon: float = Query(127.1037952, description="경도"),
+    lat: float = Query(37.499, description="위도"),
+    lon: float = Query(127.103, description="경도"),
     dog_size: DogSize = Query(DogSize.small, description="견종 크기 (small/medium/large)"),
     sensitivities: str = Query("", description="민감군 목록 (쉼표로 구분)"),
     coat_type: CoatType = Query(CoatType.double, description="모피 종류 (single/double)"),
@@ -43,8 +43,8 @@ async def get_walkability_current (
 
 @router.get("/hourly")
 async def get_walkability_hourly (
-lat: float = Query(37.6419378, description="위도"),
-    lon: float = Query(127.0170019, description="경도"),
+lat: float = Query(37.641, description="위도"),
+    lon: float = Query(127.017, description="경도"),
     hours: int = Query(12, description="시간 (0-12)", ge=1, le=12),
     dog_size: DogSize = Query(DogSize.small, description="견종 크기 (small/medium/large)"),
     sensitivities: str = Query("", description="민감군 목록 (쉼표로 구분)"),
@@ -76,8 +76,8 @@ lat: float = Query(37.6419378, description="위도"),
 
 @router.get("/weekly")
 async def get_walkability_weekly (
-    lat: float = Query(37.5665, description="위도"),
-    lon: float = Query(126.9780, description="경도"),
+    lat: float = Query(37.566, description="위도"),
+    lon: float = Query(126.978, description="경도"),
     days: int = Query(7, description="일자 (1~7)", ge=1, le=7),
     dog_size: DogSize = Query(DogSize.small, description="견종 크기 (small/medium/large)"),
     sensitivities: str = Query("", description="민감군 목록 (쉼표로 구분)"),
@@ -109,8 +109,8 @@ async def get_walkability_weekly (
     
 @router.get("/current/detail")
 async def get_walkability_current_detail (
-    lat: float = Query(37.6419399, description="위도"),
-    lon: float = Query(127.0170059, description="경도")
+    lat: float = Query(37.641, description="위도"),
+    lon: float = Query(127.017, description="경도")
     ) -> Dict[str, Any]:
     """
     현재 날씨 정보 조회
